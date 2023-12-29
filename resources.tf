45bcdf54e680db0d2974d1760bb8445059c562b9
vikas-bn <vikasbnarikkady1@gmail.com>
2023-12-29 23:01:07 +0000

    ftp port enabled

--- resources.tf
+++ resources.tf
@@ -23,6 +23,14 @@ resource "aws_security_group" "frontend_access" {
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
   
   ingress {
     from_port        = 443
